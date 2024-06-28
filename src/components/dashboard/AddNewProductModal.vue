a3e0a1944c740a4a0e06a893f33a6c18518d1620
MaximusGyraditus <kanisornputta@gmail.com>
2024-06-28 16:32:48 +0700

    Add product modal

--- src/components/dashboard/AddNewProductModal.vue
+++ src/components/dashboard/AddNewProductModal.vue
@@ -1,14 +1,14 @@
 <template>
   <div
-    class="modal modal-lg fade modal-fullscreen-md-down"
+    class="modal modal-md fade modal-fullscreen-md-down"
     id="addNewProductModal"
     tabindex="-1"
     aria-labelledby="itemModalLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
-        <form @submit.prevent="console.log('Submit')">
+        <form @submit="addProduct">
           <div class="modal-header">
             <h1 class="modal-title fs-5" id="addNewProductModalLabel">
               Adding New Product
@@ -20,50 +20,126 @@
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body" style="max-height: 90%; overflow-y: auto">
-            <form>
-              <div class="row justify-content-center">
-                <div
-                  class="col-6 col-md-4 m-4 d-flex justify-content-center align-items-center"
-                >
-                  <img
-                    src="@/assets/product.png"
-                    class="rounded-4"
-                    alt=""
-                    style="height: 30vh"
+          <div class="modal-body" style="max-height: 75vh; overflow-y: auto">
+            <div class="row justify-content-center">
+              <div
+                class="col-8 col-md-4 m-4 d-flex justify-content-center align-items-center"
+              >
+                <img
+                  src="@/assets/product.png"
+                  class="rounded-4"
+                  alt=""
+                  style="height: 30vh"
+                />
+              </div>
+
+              <div class="col-10 col-md-6">
+                <!-- Product Name -->
+                <div class="mb-3">
+                  <label for="nameInput" class="form-label"
+                    >Product Name:</label
+                  >
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="nameInput"
+                    aria-describedby="nameError"
+                    required
+                    placeholder="Product Name"
+                    v-model="productName"
                   />
+                  <div id="nameError" class="form-text">{{ nameError }}</div>
                 </div>
 
-                <div class="col-10 col-md-6">
-                  <div class="mb-3">
-                    <label for="nameInput" class="form-label"
-                      >Product Name:</label
-                    >
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="nameInput"
-                      aria-describedby="nameError"
-                      required
-                      v-model="productName"
-                    />
-                    <div id="nameError" class="form-text">nameError</div>
-                  </div>
-                  <div class="mb-3">
-                    <label for="desInput" class="form-label"
-                      >Description:</label
-                    >
-                    <textarea
-                      class="form-control description-input"
-                      id="desInput"
-                      rows="2"
-                      v-model="description"
-                    ></textarea>
-                  </div>
+                <!-- Product Description -->
+                <div class="mb-3">
+                  <label for="desInput" class="form-label">Description:</label>
+                  <textarea
+                    class="form-control description-input"
+                    id="desInput"
+                    rows="2"
+                    placeholder="Product Description"
+                    v-model="description"
+                  ></textarea>
+                </div>
+
+                <!-- Price -->
+                <div class="mb-3">
+                  <label for="priceInput" class="form-label">Price:</label>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="priceInput"
+                    aria-describedby="priceError"
+                    required
+                    placeholder="00.00"
+                    v-model="price"
+                    step="0.01"
+                  />
+                  <div id="priceError" class="form-text">{{ priceError }}</div>
                 </div>
               </div>
-            </form>
+            </div>
+
+            <!-- Under picture -->
+            <div class="row justify-content-center">
+              <!-- Category -->
+              <div class="col-10 col-md-6">
+                <label for="categorySelect" class="form-label">Category:</label>
+                <select
+                  class="form-select"
+                  id="categorySelect"
+                  aria-label="Default select example"
+                  v-model="selectedCategory"
+                >
+                  <option value="---NONE---">NONE</option>
+                  <option
+                    v-for="(category, index) in storeData.categories"
+                    :key="index"
+                    :value="category"
+                  >
+                    {{ category }}
+                  </option>
+                  <option value="---NEW---">Add New Category</option>
+                </select>
+                <div v-if="selectedCategory === '---NEW---'" class="mt-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Enter New Category"
+                    v-model="newCategory"
+                    required
+                  />
+                </div>
+              </div>
+
+              <!-- Stock -->
+              <div class="col-10 col-md-6">
+                <div>
+                  <input
+                    class="form-check-input me-2"
+                    type="checkbox"
+                    id="flexCheckDefault"
+                    v-model="isStockEnabled"
+                  />
+                  <label for="stockInput" class="form-label">Stock:</label>
+                </div>
+                <input
+                  type="number"
+                  class="form-control"
+                  id="stockInput"
+                  aria-describedby="stockError"
+                  required
+                  placeholder="0"
+                  v-model="stock"
+                  :disabled="!isStockEnabled"
+                  step="1"
+                />
+                <div id="stockError" class="form-text">{{ stockError }}</div>
+              </div>
+            </div>
           </div>
+
           <!-- Footer -->
           <div class="modal-footer" style="height: 20%">
             <button
@@ -82,17 +158,84 @@
 </template>
 
 <script>
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
 export default {
-    setup() {
-        const productName = ref("")
-        
+  setup() {
+    const store = useStore();
+    const storeData = computed(() => store.getters["auth/storeData"]);
 
-        return {
-            productName,
+    console.log(storeData.value.categories);
+
+    const productName = ref("");
+    const nameError = ref("");
+    const description = ref("");
+    const price = ref("");
+    const priceError = ref("");
+    const selectedCategory = ref("---NONE---");
+    const newCategory = ref("");
+    const isStockEnabled = ref(false);
+    const stock = ref("");
+    const stockError = ref("");
+
+    const clearForm = () => {
+        productName.value = "";
+        nameError.value = "";
+        description.value = "";
+        price.value = "";
+        priceError.value = "";
+        selectedCategory.value = "---NONE---";
+        newCategory.value = "";
+        isStockEnabled.value = false;
+        stock.value = "";
+        stockError.value = "";
+    }
+
+    const addProduct = async () => {
+
+        var category = "";
+
+        if (selectedCategory.value === "---NONE--")
+            category = "";
+        else if (selectedCategory.value === "---NEW---")
+            category = newCategory.value.trim();
+        else category = selectedCategory.value;
+
+        var saveStock = null;
+
+        if (isStockEnabled.value)
+            saveStock = stock.value;
+
+        const productDto = {
+            name: productName.value.trim(),
+            description: productName.value.trim(),
+            price: price.value,
+            category: category,
+            stock: saveStock
         }
+
+        console.log(productDto);
+
+        await store.dispatch("products/addProduct", productDto);
+        clearForm();
     }
 
 
+    return {
+      productName,
+      nameError,
+      description,
+      price,
+      priceError,
+      storeData,
+      selectedCategory,
+      newCategory,
+      isStockEnabled,
+      stock,
+      stockError,
+      addProduct,
+    };
+  },
 };
 </script>
 
--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -43,6 +43,44 @@ const actions = {
             console.error("Error config:", error.config);
             return null;
         }
+    },
+
+
+    async addProduct({rootGetters}, productDto) {
+        console.log("adding products");
+        const user = rootGetters["auth/user"];
+        console.log(user);
+        console.log(productDto);
+        if (!user.storeId) {
+            console.log("User is not linked to a store");
+            return null;
+        }
+
+        const url = `api/products/${user.storeId}/${user.userId}`; // Replace with your API endpoint
+        const token = rootGetters["auth/token"];
+        try {
+          const response = await axios.post(url, productDto,{
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          });
+          const responseProduct = response.data;
+          console.log(responseProduct);
+          return;
+        } catch (error) {
+            if (error.response) {
+                console.error("Error response:", error.response.data);
+                console.error("Error status:", error.response.status);
+                console.error("Error headers:", error.response.headers);
+            } else if (error.request) {
+                console.error("Error request:", error.request);
+            } else {
+                console.error("Error message:", error.message);
+            }
+            console.error("Error config:", error.config);
+            return null;
+        }
     }
 
 
